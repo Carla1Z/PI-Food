82cafd3f0418d9f235638f76231b00d7b3f7c1e5
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-08-29 14:06:46 -0300

    ruta dietas

--- api/src/routes/dietsRouter.js
+++ api/src/routes/dietsRouter.js
@@ -1,5 +1,18 @@
 const { Router } = require("express");
+const { Diet } = require("../db");
+
 
 const dietsRouter = Router();
+
+dietsRouter.get("", async(req, res) => {
+    try {
+        //creo una variable que busque dentro de la tabla Diets, y luego me las devuelva
+        let diets = await Diet.findAll()
+        res.status(200).send(diets)
+        // res.send("GET de rutas diets")
+    } catch (error) {
+        console.log("Error en la ruta DIETS= " + error);
+    }
+})
 
 module.exports = dietsRouter;
