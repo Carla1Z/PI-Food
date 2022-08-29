1d3f99ca134a9c59c16983ca31cf4c78143dc0cc
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-08-29 15:13:41 -0300

    react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import Form from './view/Form';
-import Home from './view/Home';
-import LandingPage from './view/LandingPage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Form from "./view/Form";
+import Home from "./view/Home";
+import LandingPage from "./view/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <Route exact path="/" component={LandingPage}/>
-      <Route path="/home" component={Home} />
-      <Route path="/form" component={Form} />
-      <Route path="/carla"><h2>Soy una ruta random</h2></Route>
-    </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/form" component={Form} />
+          <Route path="/carla">
+            <h2>Soy una ruta random</h2>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
