a400b5366dfc81d5864bc54f6bdff03bf2c6879f
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-10 21:34:58 -0300

    form

--- client/src/view/Form.jsx
+++ client/src/view/Form.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 
 export default function Form() {
+  // const [error, setError] = useState('')
+
+   
   return (
     <div>
-      <Link to="/">
+      <Link to="/home">
         <button>Inicio</button>
       </Link>
+      
       <h2>Formulario de creación de la muerte 😈</h2>
+      <form>
+        <label>Titulo: </label>
+        <input name='title' placeholder='Guiso de fideo moñito' />
+        <label>Resumen:</label>
+        <input name="summary" />
+        <label>Saludable: </label>
+        <input name="healthScore" />
+        <label>Paso a paso: </label>
+        <input name="analyzedInstructions" />
+        <button>Enviar receta</button>
+      </form>
+
+
+
     </div>
   );
 }
