ffc5a24dcb8a7c26f9263f94419a2c88a0ea1154
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-11 21:51:15 -0300

    Form

--- client/src/view/Form.jsx
+++ client/src/view/Form.jsx
@@ -30,18 +30,81 @@ export default function Form() {
         <label>Titulo: </label>
         <input
           name="title"
-          value={recipe.title}
+          type="text"
+          //   value={recipe.title}
           placeholder="Guiso de fideo moñito"
         />
         <label>Resumen:</label>
-        <input name="summary" value={recipe.summary} />
+        <input
+          name="summary"
+          type="text"
+          // value={recipe.summary}
+        />
         <label>Saludable: </label>
-        <input name="healthScore" value={recipe.healthScore} />
+        <input
+          name="healthScore"
+          type="text"
+          // value={recipe.healthScore}
+        />
         <label>Paso a paso: </label>
         <input
           name="analyzedInstructions"
-          value={recipe.analyzedInstructions}
+          type="text"
+          //   value={recipe.analyzedInstructions}
         />
+        <label>Tipo de dieta:</label>
+        <div>
+          <label>
+            <input name="gluten free" type="checkbox" />
+            Libre de gluten
+          </label>
+
+          <label>
+            <input name="dairy free" type="checkbox" />
+            Libre de lácteos
+          </label>
+
+          <label>
+            <input name="lacto ovo vegetarian" type="checkbox" />
+            Lacto ovo vegetariano
+          </label>
+
+          <label>
+            <input name="vegan" type="checkbox" />
+            Vegano
+          </label>
+
+          <label>
+            <input name="paleolithic" type="checkbox" />
+            Paleolítico
+          </label>
+
+          <label>
+            <input name="primal" type="checkbox" />
+            Primitivo
+          </label>
+
+          <label>
+            <input name="whole 30" type="checkbox" />
+            Whole 30
+          </label>
+
+          <label>
+            <input name="pescatarian" type="checkbox" />
+            Pescetariano
+          </label>
+
+          <label>
+            <input name="ketogenic" type="checkbox" />
+            Cetogenico
+          </label>
+
+          <label>
+            <input name="fodmap friendly" type="checkbox" />
+            Fodmap friendly
+          </label>
+        </div>
+
         <button>Enviar receta</button>
       </form>
     </div>
