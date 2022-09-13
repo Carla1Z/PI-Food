f9ac715bd39c8d022725952ff15a184bdecf934b
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-13 20:56:20 -0300

    landing

--- client/src/view/LandingPage.jsx
+++ client/src/view/LandingPage.jsx
@@ -4,16 +4,15 @@ import food from '../assets/food.gif'
 
 export default function LandingPage() {
   return (
-    <div>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div className={styles.gif}>
+      </div>
+      <div className={styles.henryFood}>
         <div className={styles.henry}>Henry</div>
         <div className={styles.food}>Food</div>
         <Link to="/home">
           <button className={styles.button}>Cocinar</button>
         </Link>
-      </div>
-      <div className={styles.gif}>
-        <img src={food} alt='gif animado' />
       </div>
     </div>
   );
