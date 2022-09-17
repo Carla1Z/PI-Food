265177c7554e1df1dae585bbad3932f1cbab86f4
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-17 13:32:33 -0300

    det

--- client/src/view/Detail.jsx
+++ client/src/view/Detail.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 import RecipeDetails from "../components/RecipeDetails";
 
 export default function Detail() {
   return (
     <div>
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
       {/* <p>Ruta detalle</p> */}
       <RecipeDetails />
     </div>
